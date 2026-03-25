2f1ba1c278aa1485164a07813a9e5b573b6d50e3
Thomas Furtado <tfurtado09876@gmail.com>
2026-03-25 17:17:45 -0400

    Polished up script

--- Single_cell_seq.R
+++ Single_cell_seq.R
@@ -1,6 +1,6 @@
 ##Packages----
 
-#Installation
+#Install (if needed)
 install.packages("Seurat")
 
 #Load Packages
@@ -9,11 +9,11 @@ library(clusterProfiler)
 library(org.Mm.eg.db)
 library(ggplot2)
 
-#Load Seurat Object----
+##Load Seurat Object----
+
 seu <- readRDS("seurat_ass4.rds")
 
-
-##Loading scaled Object*********
+#Load pre-scaled object (to prevent having to re-scale every session) (Overwrites previous)
 seu <- readRDS("seu_after_scaling.rds")
 
 
@@ -21,12 +21,11 @@ seu
 class(seu)
 
 
-#Inspect Structure
+##Data Inspection and QC Checks----
 
 Assays(seu)
 
 colnames(seu@meta.data)
-
 head(seu@meta.data)
 
 table(Idents(seu))
@@ -50,14 +49,22 @@ table(seu$mouse_id == "", seu$orig.ident)
 
 head(seu@meta.data[seu$mouse_id == "", ])
 
-#There are missing mouse_id values spread throughout the data set. Therefore, later on, we will use biosample_id for DE. For now, we may continue with the regular intended pipeline.
+#Note many missing mouse_id, may need to use biosample_id for DE
 
+
+##Feature Selection and Scaling----
 
 seu <- FindVariableFeatures(seu)
 length(VariableFeatures(seu))
 
 seu <- ScaleData(seu, features = VariableFeatures(seu), verbose = FALSE)
+
+#Save scaled object
 saveRDS(seu, file = "seu_after_scaling.rds", compress = TRUE)
+
+
+
+##Dimensionality Reduction and Clustering----
 
 seu <- RunPCA(seu, features = VariableFeatures(seu), verbose = FALSE)
 
@@ -65,40 +72,39 @@ ElbowPlot(seu, ndims = 30)
 #1:12 seems to be the most fitting
 
 seu <- FindNeighbors(seu, dims = 1:12, verbose = FALSE)
-
 seu <- FindClusters(seu, resolution = 0.3, verbose = FALSE)
 
 seu <- RunUMAP(seu, dims = 1:12, verbose = FALSE)
 
 
-#Visualize UMAP
+
+##UMAP Visualization and Cluster Exploration----
+
 DimPlot(seu, reduction = "umap", label = TRUE)
 table(Idents(seu))
 
 
 DimPlot(seu, reduction = "umap", group.by = "organ_custom")
-
 DimPlot(seu, reduction = "umap", group.by = "time")
 
-#Lets choose a reasonably sized cluster that also shows some time variation.
 
+
+#Lets choose a reasonably sized cluster that also shows some time variation.
 #To do this lets look at how each cluster is distributed over time.
 table(Idents(seu), seu$time)
 
-#Cluster 5 has quite a lot of variation across the studied time periods. I will choose to investiate this cluster.
-
-#What genes in Cluster 5 change between early and late infection?
-#================================================================
+#Cluster 5 has quite a lot of variation across the studied time periods. I will choose to investigate this cluster.
 
 
-#Lets subset Cluster 5
+
+##Differential Expression: Cluster 5 -> D05 vs D14----
 
 cluster5 <- subset(seu, idents = 5)
 table(cluster5$time)
 
+
 Idents(cluster5) <- cluster5$time
 
-#Finding differentially expressed genes between the two time frames.
 markers_5 <- FindMarkers(
   cluster5,
   ident.1 = "D14",
@@ -108,9 +114,16 @@ markers_5 <- FindMarkers(
 )
 
 head(markers_5)
+
 #Obp1a has very low (-310) avg_log2FC, meaning it is much higher in D05 than D14
 
 #Most of the top hits are "Rps___ / Rpl___" ribosomal genes, which are non-specific and likely dominate other meaningful results. Lets filter for biologically meaningful genes.
+
+markers_5_filtered <- markers_5[
+  markers_5$p_val_adj < 0.05 &
+    abs(markers_5$avg_log2FC) > 0.5,
+]
+
 
 markers_5_clean <- markers_5_filtered[
   !grepl("^Rps|^Rpl", rownames(markers_5_filtered)),
@@ -123,13 +136,14 @@ write.csv(markers_5_clean, "DE_results.csv")
 
 #Instinctively, cluster5 genes at D05 may be expressing immune-related genes, which come to be reduced come D14.
 
-#Visualize select genes----
+
+##Visualize select genes----
 
 select_genes <- c("Ifitm2", "Ifi27l2a", "Il7r", "Obp1a")
 FeaturePlot(cluster5, features = select_genes, min.cutoff = "q10", max.cutoff = "q90")
 
 
-#ORA----
+##Functional Enrichment (ORA)----
 
 sig_genes <- rownames(markers_5_clean)
 
@@ -140,15 +154,15 @@ ego <- enrichGO(
   ont = "bp"
 )
 
-#Visualize
 
+#Visualize
 dotplot(ego, showCategory = 9) +
   theme(
     plot.margin = margin(t = 0, r = 5, b = 0, l = 0),
     axis.text.y = element_text(size = 7, lineheight = 0.7)
   )
 
-#Annotation of cluster 5----
+##Annotation of Cluster 5----
 
 #Due to computational strain, annotation will be done using a small subset of the data while still commiting a true marker-based annotation step.
 set.seed(123)
@@ -190,7 +204,9 @@ write.csv(top15_df, "cluster5_top15_markers.csv")
 #Genes such as Cd79a, Cd79b, Ms4a1, Pax5, Igkc, Ighm, etc. are all classic B-cell markers!
 #Therefore Cluster 5 is a B-cell population
 
-#Summary conclusion----
+
+
+##Biological Interpretation and Summary----
 
 #To summarize: 
 #From Markers -> B-cell genes identified, Cluster 5 -> B-cells
@@ -198,7 +214,6 @@ write.csv(top15_df, "cluster5_top15_markers.csv")
 #From DE -> Interferon/anti-viral genes identified -> active response early (D05)
 
 #From ORA -> Immune activation, antigen presentation, viral response
-
 
 
 #Likely conclusion:
